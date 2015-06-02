5888d8305c195b621531dab15f116b8baf4631a8
Hendry <email.hendry.lee@gmail.com>
2015-06-02 21:45:27 +0700

    Can go into questions screen now

---
+++ public/templates/quiz/quizController.js
@@ -0,0 +1,40 @@
+angular.module("personaApp")
+.controller('quizController', ['$http', function($http){
+    var quiz = this;
+    quiz.started = false;
+    quiz.questionNumber = 0;
+    quiz.gender = "";
+
+    quiz.questions = [];
+    quiz.answers = [];
+
+    $http.get('/assets/quizData.json').success(function(data){
+        quiz.questions = data;
+        console.log("Successfully loaded " + quiz.questions.length + " questions");
+    });
+
+    quiz.isStarted = function(){
+        return quiz.started;
+    };
+
+    quiz.isGenderPage = function(){
+        return quiz.isStarted() && quiz.questionNumber == 0;
+    }
+
+    quiz.setGender = function(gender) {
+        quiz.gender = gender;
+    }
+
+    quiz.start = function() {
+        quiz.started = true;
+    };
+
+    quiz.showNextQuestion = function() {
+        if(!quiz.gender){
+            alert("Pilih cowo apa cewe dulu");
+            return;
+        }
+
+        quiz.questionNumber++;
+    }
+}]);
--- public/templates/quiz/quizCreator/quizCreator.js
+++ public/templates/quiz/quizCreator/quizCreator.js
@@ -1,24 +1,7 @@
 angular.module('personaApp')
 .directive('quizCreator', function() {
-	var controller = ['$http', function($http) {
-		var quiz = this;
-		var started = false;
-		var questionNumber = 0;
-		var gender = 0;
-
-		quiz.questions = [];
-		quiz.answers = [];
-
-		$http.get('/assets/quizData.json').success(function(data){
-			quiz.questions = data;
-			console.log("Successfully loaded " + quiz.questions.length + " questions");
-		});
-	}];
-
-	return {
-		restrict: 'E',
-		templateUrl:'/templates/quiz/quizCreator/quizCreator.html',
-		controller: controller,
-		controllerAs:"quiz"
-	}
+    return {
+        restrict: 'E',
+        templateUrl:'/templates/quiz/quizCreator/quizCreator.html',
+    };
 });
