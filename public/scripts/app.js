7219a86bedfce6c36a2caa902fbf224a29a95d6a
Hendry <hendry@icehousecorp.com>
2015-06-03 14:52:37 +0700

    Add titling to pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,26 +18,33 @@ angular.module("personaApp", ['ngRoute'])
   $routeProvider
 
   .when('/', {
+    title: 'Home',
     templateUrl: '/templates/home/index.html'
     })
 
   .when('/quiz', {
+    title: 'Quiz',
     templateUrl: '/templates/quiz/index.html'
   })
 
-  .when('/results', {
-    templateUrl: ''
-  })
-
   .when('/persona', {
+    title: 'Persona',
     templateUrl: '/templates/persona/index.html'
   })
 
   .when('/about', {
+    title: 'About',
     templateUrl: '/templates/about/index.html'
   })
 
   .when('/download', {
+    title: 'Download',
     templateUrl: '/templates/download/index.html'
   });
-});
+})
+
+.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    })
+}]);
