f8022582611fbe7f7f9e5269e440f81d23e9a1ed
Hendry <hendry@icehousecorp.com>
2015-06-04 19:05:11 +0700

    Add dropdowner button

--- public/templates/persona/detail/detailController.js
+++ public/templates/persona/detail/detailController.js
@@ -1,11 +1,27 @@
 angular.module("personaApp")
-.controller('detailController', ['$routeParams', function($routeParams){
+.controller('detailController', ['$routeParams, $http', function($routeParams, $http){
     var ctrl = this;
     ctrl.current = 1;
     ctrl.popInVisible = false;
+    ctrl.type = 1;
 
-    ctrl.togglePopIn = function(){
-        ctrl.popInVisible = !ctrl.popInVisible;
+    ctrl.toggleDropdown = function(){
+        ctrl.dropdownVisible = !ctrl.dropdownVisible;
+    }
+
+    ctrl.checkType = function(){
+        if(ctrl.current == 1 ||
+            ctrl.current == 8 ||
+            ctrl.current == 9){
+            ctrl.type = 1;
+        }
+
+        else if(ctrl.current == 2 ||
+            ctrl.current == 3 ||
+            ctrl.current == 4){
+            ctrl.type = 2;
+        }
+        else ctrl.type = 3;
     }
 
     ctrl.previous = function(){
@@ -28,5 +44,6 @@ angular.module("personaApp")
 
     $(document).ready(function() {
         document.title = "9Persona - Tipe " + ctrl.current;
+        ctrl.checkType();
     });
 }]);
