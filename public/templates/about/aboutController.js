51df23bb8f415d2258d645e4b303e9abc2b2150b
Hendry <email.hendry.lee@gmail.com>
2015-06-28 20:35:44 +0700

    Make fade in working

--- public/templates/about/aboutController.js
+++ public/templates/about/aboutController.js
@@ -3,12 +3,75 @@ angular.module("personaApp")
     var ctrl = this;
 
     ctrl.status = 1;
+    ctrl.previous = 0;
+
+    ctrl.previousAnim = '';
+    ctrl.currentAnim = '';
 
     ctrl.getStatus = function(stat) {
         return ctrl.status === stat;
     }
 
+    ctrl.getPrevious = function(stat) {
+        return ctrl.previous === stat;
+    }
+
     ctrl.setStatus = function(stat) {
+        ctrl.previous = ctrl.status;
         ctrl.status = stat;
+    }
+
+    ctrl.shouldFadeInLeft = function(stat) {
+        var result = false;
+
+        if(ctrl.getStatus(stat)){
+            if(stat < ctrl.previous) {
+                result = true;
+                console.log(stat + " shouldFadeInLeft " + result);
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    ctrl.shouldFadeInRight = function(stat) {
+        var result = false;
+
+        if(ctrl.getStatus(stat)){
+            if(stat > ctrl.previous) {
+                result = true;
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    ctrl.shouldFadeOutLeft = function(stat) {
+        var result = false;
+        console.log(ctrl.getPrevious(stat))
+        if(ctrl.getPrevious(stat)) {
+            if(stat < ctrl.status) {
+                result = true; 
+            }
+        } else {
+            result = false;
+        }
+        
+        return result;
+    }
+    
+    ctrl.shouldFadeOutRight = function(stat) {
+        var result = false;
+
+         if(ctrl.getPrevious(stat)) {
+            if(stat > ctrl.status) {
+                result = true;
+            }
+        } else {
+            result = false;
+        }
+        return result;
     }
 }]);
