1ca9a74c103326dcba6f259e04aec6b90d23a6ae
Hendry <email.hendry.lee@gmail.com>
2015-06-28 18:01:53 +0700

    Make buttons work

--- public/templates/about/aboutController.js
+++ public/templates/about/aboutController.js
@@ -1,4 +1,14 @@
 angular.module("personaApp")
 .controller('aboutController', ['$animate', function($animate){
+    var ctrl = this;
 
+    ctrl.status = 1;
+
+    ctrl.getStatus = function(stat) {
+        return ctrl.status === stat;
+    }
+
+    ctrl.setStatus = function(stat) {
+        ctrl.status = stat;
+    }
 }]);
